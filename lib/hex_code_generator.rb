64c20d4f944c0d258f7065c37f49c682ddddb8e3
Ka Mok <heykamok@gmail.com>
2016-04-25 21:41:44 -0400

    Add Travis.ci and refractor

--- lib/hex_code_generator.rb
+++ lib/hex_code_generator.rb
@@ -2,8 +2,10 @@ require "hex_code_generator/version"
 
 class HexGenerator 
 
+  VALID_HEX_VALUES = %w(a b c d e f 1 2 3 4 5 6 7 8 9)
+
   def self.new_hex
-    "#".tap { |hex| 6.times { hex << %w(a b c d e f 1 2 3 4 5 6 7 8 9).sample } } 
+    "#".tap { |pound_sign| 6.times { pound_sign << VALID_HEX_VALUES.sample } } 
   end
 
 end 
