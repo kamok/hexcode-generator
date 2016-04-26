6d2852cb342839f00d00462cd0210389976a3554
Ka Mok <heykamok@gmail.com>
2016-04-25 22:31:49 -0400

    Unit test new_hex

--- lib/hex_code_generator.rb
+++ lib/hex_code_generator.rb
@@ -2,7 +2,7 @@ require "hex_code_generator/version"
 
 class HexGenerator 
 
-  VALID_HEX_VALUES = %w(a b c d e f 1 2 3 4 5 6 7 8 9)
+  VALID_HEX_VALUES = %w(a b c d e f 0 1 2 3 4 5 6 7 8 9)
 
   def self.new_hex
     "#".tap { |pound_sign| 6.times { pound_sign << VALID_HEX_VALUES.sample } } 
--- spec/hex_code_generator_spec.rb
+++ spec/hex_code_generator_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 require 'hex_code_generator'
 
 describe HexCodeGenerator do
+
   it 'has a version number' do
     expect(HexCodeGenerator::VERSION).not_to be nil
   end
+
 end
 
 describe HexGenerator do
-  it 'has a pound sign plus six characters' do
-    expect(HexGenerator.new_hex[0]).to eq("#")
-    expect(HexGenerator.new_hex.length) == 7
-  end
-end
 
+  hex = HexGenerator.new_hex
+  it 'has a pound sign plus six characters' do
+    expect(hex).to start_with "#"
+    expect(hex.length).to eq 7
+  end
+
+  it 'outputs valid hex values' do
+    valid_hex_values = HexGenerator::VALID_HEX_VALUES
+    hex_without_pound = hex[1..-1].split("") 
+    hex_without_pound.each { |char| expect(valid_hex_values).to include char }
+  end
+
+end
 
