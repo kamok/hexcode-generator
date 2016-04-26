59929fe20c3dc272f33685af48f3d41808f4ff77
Ka Mok <heykamok@gmail.com>
2016-04-25 20:06:23 -0400

    add basic tests

--- spec/hex_code_generator_spec.rb
+++ spec/hex_code_generator_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
+require 'hex_code_generator'
 
 describe HexCodeGenerator do
   it 'has a version number' do
     expect(HexCodeGenerator::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe HexGenerator do
+  it 'has a pound sign plus six characters' do
+    expect(HexGenerator.new_hex[0]).to eq("#")
+    expect(HexGenerator.new_hex.length) == 7
   end
 end
+
+
